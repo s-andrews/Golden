6383102bd128b0c7517bafc50626fd832ef60a5d
Simon Andrews <simon.andrews@babraham.ac.uk>
2017-11-24 17:02:06 +0000

    Link front end to the database

--- views.py
+++ views.py
@@ -1,27 +1,11 @@
 from Golden import app
 from flask import render_template 
+from Golden import models
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-    studies = [
-        {
-            'name' : 'Abad_2013',
-            'date' : 'Feb 12 2013',
-            'title': 'Reprogramming in vivo produces teratomas and iPS cells with totipotency features.'
-        },
-        {
-            'name' : 'Adachi_2013',
-            'date' : 'Mar 14 2013',
-            'title': 'Context-Dependent Wiring of Sox2 Regulatory Networks for Self-Renewal of Embryonic and Trophoblast Stem Cells.'
-        },
-        {
-            'name' : 'Aloia_2014',
-            'date' : 'July 23 2014',
-            'title': 'Zrf1 is required to establish and maintain neural progenitor identity.'
-        },
-
-    ]
+    studies = models.Study.query.all()
 
     return render_template('index.html',studies=studies)
