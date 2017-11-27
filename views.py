197f8a950b920d79c637be5d641469c8caec04c3
Simon Andrews <simon.andrews@babraham.ac.uk>
2017-11-27 17:32:39 +0000

    Made new GEO querying and lookup work

--- views.py
+++ views.py
@@ -1,11 +1,30 @@
 from Golden import app
 from flask import render_template 
 from Golden import models
+from .forms import NewGEOForm
+from Golden.eutils import GEODataSet
 
 @app.route('/')
 @app.route('/index')
 def index():
 
     studies = models.Study.query.all()
-
     return render_template('index.html',studies=studies)
+
+
+@app.route('/add', methods=('GET','POST'))
+def add():
+
+    form = NewGEOForm()
+    if form.validate_on_submit():
+        return render_template('new_geo.html',
+                               title='Add AOSIDOASIUD',
+                               form=form,
+                               study=GEODataSet(form.accession.data))
+
+    else:
+        return render_template('new_geo.html',
+                               title='Add new study',
+                               form=form)
+
+
